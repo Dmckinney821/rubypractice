36ad8743831c9b779465015256da815a6d28e992
Dan McKinney <3dhdaniel@gmail.com>
2019-10-28 18:10:13 -0400

    integrated new gem into rails app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,8 @@ module ApplicationHelper
 				content_tag(:p, greeting, class: 'source-greeting')
 		end
 	end
+
+	def copyright_generator
+		MckinneyViewTool::Renderer.copyright 'Dan McKinney', 'All rights reserved'
+	end
 end
